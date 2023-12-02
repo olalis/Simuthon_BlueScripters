31c9146b08660ae75cbc03515cd06be58da3e0a7
Karol <karolpl2000@onet.pl>
2023-12-02 07:32:27 +0100

    Detector implementation

--- Task2/check_size.m
+++ Task2/check_size.m
@@ -1,11 +1,17 @@
 function [result] = check_size(original, calculated)
     result = 1;
+    size_calc = size(calculated);
+    if (size_calc(1) ~= 1 || size_calc(2) ~= 4)
+        result = 0;
+        return;
+    end
+
     x_min_diff = abs(original(1) - calculated(1));
     y_min_diff = abs(original(2) - calculated(2));
     len_x_diff = abs(original(3) - calculated(3));
     len_y_diff = abs(original(4) - calculated(4));
 
-    if (len_y_diff + len_x_diff > 10)
+    if (len_y_diff > 10 || len_x_diff > 10)
         result = 0;
     end
     if (x_min_diff > 10 || y_min_diff > 10)
