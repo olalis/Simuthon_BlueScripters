4bb6b405f1c3bb48bf48b1c0e00e81c7dfe07777
Karipolska <61945155+Karipolska@users.noreply.github.com>
2023-12-02 07:42:56 +0100

    Update Task_2_Main.m

--- Task2/Task_2_Main.m
+++ Task2/Task_2_Main.m
@@ -5,9 +5,7 @@ clc; clear all; close all;
 % variable 'mat_file_name' should be adjusted to 
 % .mat file withBoundingBoxes
 folder_path = "C:\Users\karol\Desktop\Simuthon\Simuthon_BlueScripters\Task2\Training Dataset";
-mat_file_name = "Task_2_Training_Dataset.mat";
 addpath(folder_path);
-load(mat_file_name);
 load('detector.mat');
 
 %% Load image database
@@ -16,7 +14,6 @@ dataImageDataBase = imageDatastore(folder_path, "FileExtensions", ".png");
 %% Variables init
 num_image = length(dataImageDataBase.Files);
 calculatedBoundingBox = repmat({zeros(1, 4)}, 1, num_image);
-BoundingBox = extract_boxes(Task_2_Training_Data);
 
 %% Detection using ACF detector
 for i = 1:num_image
@@ -28,5 +25,6 @@ for i = 1:num_image
 end
 
 %% Save output
+saveToFile = saveToFile';
 save("Task_2_BlueScripters.mat", "saveToFile")
 
